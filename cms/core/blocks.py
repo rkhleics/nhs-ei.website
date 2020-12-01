1941ab1e1387d8341218301689e726225cac928b
Nick Moreton <nickmoreton@me.com>
2020-12-01 11:30:12 +0000

    removed un-needed method

--- cms/core/blocks.py
+++ cms/core/blocks.py
@@ -26,10 +26,6 @@ from wagtailnhsukfrontend.blocks import (
     SummaryListBlock,
 )
 
-def pretty_size(n,pow=0,b=1024,u='B',pre=['']+[p+'i'for p in'KMGTPEZY']):
-    pow,n=min(int(log(max(n*b**pow,1),b)),len(pre)-1),n*b**pow
-    return "%%.%if %%s%%s"%abs(pow%(-pow-1))%(n/b**float(pow),pre[pow],u)
-
 
 class RecentPostsBlock(FlattenValueContext, StructBlock):
     title = CharBlock()
