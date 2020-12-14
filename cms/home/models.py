3c26f62d35d28e1b2e0f63a9411bd8e32ca2812b
Nick Moreton <nickmoreton@me.com>
2020-12-14 11:42:24 +0000

    deleted homepage fields not required

--- cms/home/models.py
+++ cms/home/models.py
@@ -12,42 +12,42 @@ from wagtail.images.edit_handlers import ImageChooserPanel
 class HomePage(Page):
     max_num = 1
     body = RichTextField(blank=True)
-    body_image = models.ForeignKey(
-        'wagtailimages.Image',
-        related_name='+',
-        blank=True, null=True,
-        on_delete=models.SET_NULL
-    )
-    all_news_page = models.ForeignKey(
-        'wagtailcore.Page',
-        on_delete=models.SET_NULL,
-        null=True, blank=True,
-        related_name='+',
-    )
-    all_news_title = models.CharField(max_length=100, blank=True)
-    all_news_sub_title = RichTextField(blank=True)
-    all_publications_page = models.ForeignKey(
-        'wagtailcore.Page',
-        on_delete=models.SET_NULL,
-        null=True, blank=True,
-        related_name='+'
-    )
-    all_publications_title = models.CharField(max_length=100, blank=True)
-    all_publications_sub_title = RichTextField(blank=True)
+    # body_image = models.ForeignKey(
+    #     'wagtailimages.Image',
+    #     related_name='+',
+    #     blank=True, null=True,
+    #     on_delete=models.SET_NULL
+    # )
+    # all_news_page = models.ForeignKey(
+    #     'wagtailcore.Page',
+    #     on_delete=models.SET_NULL,
+    #     null=True, blank=True,
+    #     related_name='+',
+    # )
+    # all_news_title = models.CharField(max_length=100, blank=True)
+    # all_news_sub_title = RichTextField(blank=True)
+    # all_publications_page = models.ForeignKey(
+    #     'wagtailcore.Page',
+    #     on_delete=models.SET_NULL,
+    #     null=True, blank=True,
+    #     related_name='+'
+    # )
+    # all_publications_title = models.CharField(max_length=100, blank=True)
+    # all_publications_sub_title = RichTextField(blank=True)
 
     content_panels = Page.content_panels + [
         MultiFieldPanel([
             FieldPanel('body'),
-            ImageChooserPanel('body_image'),
+            # ImageChooserPanel('body_image'),
         ], heading='Main Body'),
-        MultiFieldPanel([
-            PageChooserPanel('all_news_page'),
-            FieldPanel('all_news_title'),
-            FieldPanel('all_news_sub_title'),
-        ], heading='Latest News'),
-        MultiFieldPanel([
-            PageChooserPanel('all_publications_page'),
-            FieldPanel('all_publications_title'),
-            FieldPanel('all_publications_sub_title'),
-        ], heading='Latest Publications'),
+        # MultiFieldPanel([
+            # PageChooserPanel('all_news_page'),
+            # FieldPanel('all_news_title'),
+            # FieldPanel('all_news_sub_title'),
+        # ], heading='Latest News'),
+        # MultiFieldPanel([
+            # PageChooserPanel('all_publications_page'),
+            # FieldPanel('all_publications_title'),
+            # FieldPanel('all_publications_sub_title'),
+        # ], heading='Latest Publications'),
     ]
